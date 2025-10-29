ac557e8e0c7b966825e6197e06bf70f444f1810e
Paul Rammone <89.pablo.alvarez@gmail.com>
2025-10-29 16:06:25 -0300

    Cambio "About" por "AboutUs" y le agrego un poco mas de contenido.

--- kioscoya/src/pages/AboutUs.jsx
+++ kioscoya/src/pages/AboutUs.jsx
@@ -1,3 +1,8 @@
-export default function About() {
-  return <h1>Acerca de nosotros</h1>;
+export default function AboutUs() {
+  return (
+    <>
+      <h1>Acerca de nosotros</h1>
+      <p>Somos un kiosco de barrio con productos clásicos y modernos.</p>
+    </>
+  )
 }
