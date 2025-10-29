0e80af4493e35a40e88947b4fb83db71426632e8
Paul Rammone <89.pablo.alvarez@gmail.com>
2025-10-29 17:27:16 -0300

    Agrego los iconos de bootstrap en package.json, armo el icono del cart con un índice indicador de la cantidad de items que tiene (hardcodeado, de momento), agrego la imagen del logo del e-commerce "Kiosco" y agrego los estilos del icono y del header.

---
+++ kioscoya/src/components/cart/CartIcon.jsx
@@ -0,0 +1,13 @@
+import 'bootstrap-icons/font/bootstrap-icons.css'
+import '../../App.css'
+
+export default function CartIcon() {
+  const itemCount = 3 // hardcodeado por ahora
+
+  return (
+    <div className="cart-icon-container">
+      <i className="bi bi-cart3 cart-icon"></i>
+      <span className="cart-badge">{itemCount}</span>
+    </div>
+  )
+}
--- kioscoya/src/components/layout/Header.jsx
+++ kioscoya/src/components/layout/Header.jsx
@@ -1,9 +1,18 @@
+import logo from '../../assets/images/logo_sin_fondo.png'
 import NavBar from '../navbar/NavBar'
+import CartIcon from '../cart/CartIcon'
+import '../../App.css'
 
 export default function Header() {
   return (
-    <header>
-      <NavBar />
+    <header className="header">
+      <img src={logo} alt="Logo Kiosco" className="header-logo" />
+      <div className="header-nav">
+        <NavBar />
+      </div>
+      <div className="header-cart">
+        <CartIcon />
+      </div>
     </header>
   )
 }
