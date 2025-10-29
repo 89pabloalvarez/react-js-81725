37cddbacbf544cf753e379fba65cf50c8be47712
Paul Rammone <89.pablo.alvarez@gmail.com>
2025-10-29 16:07:02 -0300

    Le pongo un poco mas de datos a Home para que no quede en solamente un titulo, le pongo un párrafo también.

--- kioscoya/src/pages/Home.jsx
+++ kioscoya/src/pages/Home.jsx
@@ -1,3 +1,8 @@
 export default function Home() {
-  return <h1>Bienvenido al Kiosco</h1>;
+  return (
+    <>
+      <h1>Bienvenido al Kiosco</h1>
+      <p>Acá vas a encontrar alfajores, caramelos, gaseosas y más.</p>
+    </>
+  )
 }
