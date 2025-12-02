93ac1da87267e92ed8748b6d07483934e9c73453
Paul Rammone <89.pablo.alvarez@gmail.com>
2025-12-01 23:03:41 -0300

    agrego el context de cartProvider debajo de la capa de browser router para tenerlo disponibilizado en todos los componentes internos.

--- src/App.jsx
+++ src/App.jsx
@@ -4,31 +4,34 @@ import Footer from './components/footer/Footer'
 import AboutUs from './pages/AboutUs'
 import HowToBuy from './pages/HowToBuy'
 import NotFound from './pages/NotFound'
-import Home from './pages/Home'
+import ProductListView from './pages/ProductListView'
 import ProductDetailContainer from './components/main/product/ProductDetailContainer'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
+import { CartProvider } from './context/CartContext'
 
 function App() {
   return (
     <BrowserRouter>
-      <Header />
-      <div className="content-layout">
-        <CategoryNavContainer />
-        <main>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="*" element={<NotFound />} />
-            <Route path="/about-us" element={<AboutUs />} />
-            <Route path="/how-to-buy" element={<HowToBuy />} />
-            <Route path="/category/:category" element={<Home />} />
-            <Route path="/product/:id" element={<ProductDetailContainer />} />
-            {/* funcion que definiré para la entrega final, que se ejecutará al usar el buscador del header. por ahora seguirá siendo not found */}
-            <Route path="/searched-product/:searched-text" element={<NotFound />} />
-          </Routes>
-        </main>
-      </div>
-      <Footer />
+      <CartProvider>
+        <Header />
+        <div className="content-layout">
+          <CategoryNavContainer />
+          <main>
+            <Routes>
+              <Route path="/" element={<ProductListView title={'¡¡Bienvenido a tu Kiosco Online!!'} subtitle={'Encuentra los productos de las mejores marcas en ésta aplicación.'} />} />
+              <Route path="*" element={<NotFound />} />
+              <Route path="/about-us" element={<AboutUs />} />
+              <Route path="/how-to-buy" element={<HowToBuy />} />
+              <Route path="/category/:category" element={<ProductListView title={'Usted se encuentra en la categoría:'} />} />
+              <Route path="/product/:id" element={<ProductDetailContainer />} />
+              <Route path="/searched-product/:searchedText" element={<ProductListView title={'Resultados de su búsqueda con el texto:'} />} />
+              {/*<Route path="/cart" element={<CardContainer />} />*/}
+            </Routes>
+          </main>
+        </div>
+        <Footer />
+      </CartProvider>
     </BrowserRouter>
   )
 }
