a4ac07103a52126ab5c86e2416a3003d30f9eb75
Paul Rammone <89.pablo.alvarez@gmail.com>
2025-12-07 14:06:18 -0300

    Reutilizo el componente CustomAlert en LoaderComponent porque habia código duplicado. cumpliendo un poco con DRY.

--- src/components/main/LoaderComponent.jsx
+++ src/components/main/LoaderComponent.jsx
@@ -1,19 +1,17 @@
 import { Spinner, Alert } from 'react-bootstrap'
 
-const LoaderComponent = ({ message = "Cargando..." }) => {
+const LoaderComponent = ({ message = 'Cargando...' }) => {
   return (
-    <div className="d-flex flex-column align-items-center mt-5">
-      <Spinner animation="border" role="status" />
-      <Alert variant="warning" className="text-center mt-3">
-        {message}
-      </Alert>
+    <div className='d-flex flex-column align-items-center mt-5'>
+      <Spinner animation='border' role='status' />
+      {CustomAlert({ variant: 'warning', message })}
     </div>
   )
 }
 
-export const customAlert = ({ variant = "info", message = "Cargando..." }) => {
+export const CustomAlert = ({ variant = 'info', message = 'Cargando...' }) => {
   return (
-    <Alert variant={variant} className="text-center mt-3">
+    <Alert variant={variant} className='text-center mt-3'>
       {message}
     </Alert>
   )
