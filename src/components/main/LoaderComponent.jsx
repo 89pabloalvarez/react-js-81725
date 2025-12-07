d65457904169caa87994491175ea2387d8d99014
Paul Rammone <89.pablo.alvarez@gmail.com>
2025-12-07 13:57:37 -0300

    Modifico el componente LoaderComponent con un mensaje por defecto y agrego una nueva funcion de "customAlert" que complementa el loader

--- src/components/main/LoaderComponent.jsx
+++ src/components/main/LoaderComponent.jsx
@@ -1,12 +1,21 @@
-import { Alert, Spinner } from 'react-bootstrap'
+import { Spinner, Alert } from 'react-bootstrap'
 
-const LoaderComponent = ({text}) => {
+const LoaderComponent = ({ message = "Cargando..." }) => {
   return (
-    <div className="d-flex justify-content-center mt-5">
-        <Spinner animation="border" role="status">
-            <span className="visually-hidden">{text}</span>
-        </Spinner>
+    <div className="d-flex flex-column align-items-center mt-5">
+      <Spinner animation="border" role="status" />
+      <Alert variant="warning" className="text-center mt-3">
+        {message}
+      </Alert>
     </div>
+  )
+}
+
+export const customAlert = ({ variant = "info", message = "Cargando..." }) => {
+  return (
+    <Alert variant={variant} className="text-center mt-3">
+      {message}
+    </Alert>
   )
 }
 
