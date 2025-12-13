2c0fe9b8247123ea001aad0bf7d9e91597794d62
Paul Rammone <89.pablo.alvarez@gmail.com>
2025-12-13 12:13:49 -0300

    FIX .env VARIABLES, resulta que si no arrancan con "VITE_" no funciona.

--- src/services/firebase.jsx
+++ src/services/firebase.jsx
@@ -3,12 +3,12 @@ import { getFirestore } from "firebase/firestore"
 
 // Firebase configuration
 const firebaseConfig = {
-  apiKey: import.meta.env.FIREBASE_API_KEY,
-  authDomain: import.meta.env.FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.FIREBASE_MESSAGING_SENDER,
-  appId: import.meta.env.FIREBASE_APP_ID
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER,
+  appId: import.meta.env.VITE_APP_ID
 }
 
 // Initialize Firebase
